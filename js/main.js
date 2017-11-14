7ba86fe25d3fe4cdfee5497a7dc31ac0c54c018c
wanyanxx <33455930+wanyanxx@users.noreply.github.com>
2017-11-14 18:36:07 +0800

    停止FSL
    function changePizzaSizes(size) {
        var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
        var newwidth =( randomPizzas.offsetWidth + dx ) +'px';
        for(var i = 0;;i<randomPizzas;i++){
          randomPizzas.style.width = newwidth;
        }
        /* for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
          var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
          var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
          document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
        } */
      }

--- js/main.js
+++ js/main.js
@@ -445,11 +445,16 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
+    var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+    var newwidth =( randomPizzas.offsetWidth + dx ) +'px';
+    for(var i = 0;;i<randomPizzas;i++){
+      randomPizzas.style.width = newwidth;
+    }
+    /* for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
       var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
       var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
       document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
-    }
+    } */
   }
 
   changePizzaSizes(size);
